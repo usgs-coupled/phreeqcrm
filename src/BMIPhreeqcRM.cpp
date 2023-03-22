3661fd61c6ac254375f244595988321868eaf481
David Parkhurst <dlparkhurst@gmail.com>
2023-03-22 13:17:21 -0600

    ready for testing

--- src/BMIPhreeqcRM.cpp
+++ src/BMIPhreeqcRM.cpp
@@ -6,6 +6,33 @@
 #include <iostream>
 #include <iomanip>
 #include <sstream>
+void Components_var(BMIPhreeqcRM& brm_ref);
+void ComponentCount_var(BMIPhreeqcRM& brm_ref);
+void Concentrations_var(BMIPhreeqcRM& brm_ref);
+void Density_var(BMIPhreeqcRM& brm_ref);
+void ErrorString_var(BMIPhreeqcRM& brm_ref);
+void FilePrefix_var(BMIPhreeqcRM& brm_ref);
+void Gfw_var(BMIPhreeqcRM& brm_ref);
+void GridCellCount_var(BMIPhreeqcRM& brm_ref);
+void NthSelectedOutput_var(BMIPhreeqcRM& brm_ref);
+void Saturation_var(BMIPhreeqcRM& brm_ref);
+void SelectedOutput_var(BMIPhreeqcRM& brm_ref);
+void SelectedOutputColumnCount_var(BMIPhreeqcRM& brm_ref);
+void SelectedOutputCount_var(BMIPhreeqcRM& brm_ref);
+void SelectedOutputHeadings_var(BMIPhreeqcRM& brm_ref);
+void SelectedOutputRowCount_var(BMIPhreeqcRM& brm_ref);
+void SolutionVolume_var(BMIPhreeqcRM& brm_ref);
+void Time_var(BMIPhreeqcRM& brm_ref);
+void TimeStep_var(BMIPhreeqcRM& brm_ref);
+void CurrentSelectedOutputUserNumber_var(BMIPhreeqcRM& brm_ref);
+void Porosity_var(BMIPhreeqcRM& brm_ref);
+void Pressure_var(BMIPhreeqcRM& brm_ref);
+void SelectedOutputOn_var(BMIPhreeqcRM& brm_ref);
+void Temperature_var(BMIPhreeqcRM& brm_ref);
+void InputVarNames_var(BMIPhreeqcRM& brm_ref);
+void OutputVarNames_var(BMIPhreeqcRM& brm_ref);
+
+
 void BMI_Variant::Clear()
 {
 	bmi_var = BMI_Var("", "", "", false, false, 0, 0);
@@ -25,7 +52,31 @@ void FilePrefix_var(BMIPhreeqcRM& brm_ref);
 BMIPhreeqcRM::BMIPhreeqcRM(int nxyz, int nthreads) :
 PhreeqcRM(nxyz, nthreads) 
 {
-	varfn_map["fileprefix"] = &FilePrefix_var;
+	varfn_map["Components"] = &Components_var;
+	varfn_map["ComponentCount"] = &ComponentCount_var;
+	varfn_map["Concentrations"] = &Concentrations_var;
+	varfn_map["Density"] = &Density_var;
+	varfn_map["ErrorString"] = &ErrorString_var;
+	varfn_map["FilePrefix"] = &FilePrefix_var;
+	varfn_map["Gfw"] = &Gfw_var;
+	varfn_map["GridCellCount"] = &GridCellCount_var;
+	varfn_map["NthSelectedOutput"] = &NthSelectedOutput_var;
+	varfn_map["Saturation"] = &Saturation_var;
+	varfn_map["SelectedOutput"] = &SelectedOutput_var;
+	varfn_map["SelectedOutputColumnCount"] = &SelectedOutputColumnCount_var;
+	varfn_map["SelectedOutputCount"] = &SelectedOutputCount_var;
+	varfn_map["SelectedOutputHeadings"] = &SelectedOutputHeadings_var;
+	varfn_map["SelectedOutputRowCount"] = &SelectedOutputRowCount_var;
+	varfn_map["SolutionVolume"] = &SolutionVolume_var;
+	varfn_map["Time"] = &Time_var;
+	varfn_map["TimeStep"] = &TimeStep_var;
+	varfn_map["CurrentSelectedOutputUserNumber"] = &CurrentSelectedOutputUserNumber_var;
+	varfn_map["Porosity"] = &Porosity_var;
+	varfn_map["Pressure"] = &Pressure_var;
+	varfn_map["SelectedOutputOn"] = &SelectedOutputOn_var;
+	varfn_map["Temperature"] = &Temperature_var;
+	varfn_map["InputVarNames"] = &InputVarNames_var;
+	varfn_map["OutputVarNames"] = &OutputVarNames_var;
 	this->task = BMIPhreeqcRM::BMI_TASKS::no_op;
 }
 // Model control functions.
@@ -797,15 +848,121 @@ void CurrentSelectedOutputUserNumber_var(BMIPhreeqcRM& brm_ref)
 		break;
 	}
 }
-//bmi_var_map["porosity"] = BMI_Var("Porosity", "double,1d", "unitless", true, true);
+void Porosity_var(BMIPhreeqcRM& brm_ref)
+{
+	int Itemsize = sizeof(double);
+	int Nbytes = Itemsize * brm_ref.GetGridCellCount();
+	//name, type, std::string units, set, get, Nbytes, Itemsize  
+	BMI_Var bv = BMI_Var("Porosity", "double,1d", "unitless", true, true, Nbytes, Itemsize);
+	brm_ref.bmi_variant.bmi_var = bv;
+	switch (brm_ref.task)
+	{
+	case BMIPhreeqcRM::BMI_TASKS::GetVar:
+		brm_ref.bmi_variant.DoubleVector = brm_ref.GetPorosity();
+		break;
+	case BMIPhreeqcRM::BMI_TASKS::SetVar:
+		brm_ref.SetPorosity(brm_ref.bmi_variant.DoubleVector);
+		break;
+	}
+}
+void Pressure_var(BMIPhreeqcRM& brm_ref)
+{
+	int Itemsize = sizeof(double);
+	int Nbytes = Itemsize * brm_ref.GetGridCellCount();
+	//name, type, std::string units, set, get, Nbytes, Itemsize  
+	BMI_Var bv = BMI_Var("Pressure", "double,1d", "atm", true, true, Nbytes, Itemsize);
+	brm_ref.bmi_variant.bmi_var = bv;
+	switch (brm_ref.task)
+	{
+	case BMIPhreeqcRM::BMI_TASKS::GetVar:
+		brm_ref.bmi_variant.DoubleVector = brm_ref.GetPressure();
+		break;
+	case BMIPhreeqcRM::BMI_TASKS::SetVar:
+		brm_ref.SetPressure(brm_ref.bmi_variant.DoubleVector);
+		break;
+	}
+}
+void SelectedOutputOn_var(BMIPhreeqcRM& brm_ref)
+{
+	int Itemsize = (int)sizeof(int);
+	int Nbytes = (int)sizeof(int);
+	//name, type, std::string units, set, get, Nbytes, Itemsize
+	BMI_Var bv = BMI_Var("SelectedOutputOn", "logical", "flag", true, true, Nbytes, Itemsize);
+	brm_ref.bmi_variant.bmi_var = bv;
+	switch (brm_ref.task)
+	{
+	case BMIPhreeqcRM::BMI_TASKS::GetVar:
+		brm_ref.bmi_variant.b_var = brm_ref.GetSelectedOutputOn();
+		break;
+	case BMIPhreeqcRM::BMI_TASKS::SetVar:
+		brm_ref.SetSelectedOutputOn(brm_ref.bmi_variant.b_var);
+		break;
+	}
+}
+void Temperature_var(BMIPhreeqcRM& brm_ref)
+{
+	int Itemsize = sizeof(double);
+	int Nbytes = Itemsize * brm_ref.GetGridCellCount();
+	//name, type, std::string units, set, get, Nbytes, Itemsize  
+	BMI_Var bv = BMI_Var("Temperature", "double,1d", "C", true, true, Nbytes, Itemsize);
+	brm_ref.bmi_variant.bmi_var = bv;
+	switch (brm_ref.task)
+	{
+	case BMIPhreeqcRM::BMI_TASKS::GetVar:
+		brm_ref.bmi_variant.DoubleVector = brm_ref.GetTemperature();
+		break;
+	case BMIPhreeqcRM::BMI_TASKS::SetVar:
+		brm_ref.SetTemperature(brm_ref.bmi_variant.DoubleVector);
+		break;
+	}
+}
+void InputVarNames_var(BMIPhreeqcRM& brm_ref)
+{
+	std::vector<std::string> headings = brm_ref.GetInputVarNames();
+	size_t size = 0;
+	for (size_t i = 0; i < headings.size(); i++)
+	{
+		if (headings[i].size() > size) size = headings[i].size();
+	}
+	int Itemsize = (int)size;
+	int Nbytes = (int)(size * headings.size());
+	//name, type, std::string units, set, get, Nbytes, Itemsize
+	BMI_Var bv = BMI_Var("InputVarNames", "character,1d", "string", false, true, Nbytes, Itemsize);
+	brm_ref.bmi_variant.bmi_var = bv;
+	switch (brm_ref.task)
+	{
+	case BMIPhreeqcRM::BMI_TASKS::GetVar:
+		brm_ref.bmi_variant.StringVector = brm_ref.GetInputVarNames();
+		break;
+	case BMIPhreeqcRM::BMI_TASKS::SetVar:
+		brm_ref.bmi_variant.NotImplemented = true;
+		break;
+	}
+}
 
-
-
-
-
-
-
-
+void OutputVarNames_var(BMIPhreeqcRM& brm_ref)
+{
+	std::vector<std::string> headings = brm_ref.GetInputVarNames();
+	size_t size = 0;
+	for (size_t i = 0; i < headings.size(); i++)
+	{
+		if (headings[i].size() > size) size = headings[i].size();
+	}
+	int Itemsize = (int)size;
+	int Nbytes = (int)(size * headings.size());
+	//name, type, std::string units, set, get, Nbytes, Itemsize
+	BMI_Var bv = BMI_Var("OutputVarNames", "character,1d", "string", false, true, Nbytes, Itemsize);
+	brm_ref.bmi_variant.bmi_var = bv;
+	switch (brm_ref.task)
+	{
+	case BMIPhreeqcRM::BMI_TASKS::GetVar:
+		brm_ref.bmi_variant.StringVector = brm_ref.GetOutputVarNames();
+		break;
+	case BMIPhreeqcRM::BMI_TASKS::SetVar:
+		brm_ref.bmi_variant.NotImplemented = true;
+		break;
+	}
+}
 //////////////////
 
 
