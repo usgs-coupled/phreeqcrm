3914eca60c9ce61846b1d07981d1f6ba41af20a1
Charlton, Scott R <charlton@usgs.gov>
2023-04-04 16:50:50 -0600

    34 add pkg config to autotools and cmake distribution (#36)
    * Implemented pkg-config for autotools and cmake builds
    
    * Added phreeqcrm.cmake.in to EXTRA_DIST
    
    * Fixed libphreeqcrm_la_SOURCES
    
    * Added ldconfig to linux when building shared libs
    
    * Added print phreeqcrm.pc step
    
    * Test install using env.BUILD_TYPE
    
    * Added toolchain option to cmake install test
    
    * Added path to yaml-cpp.dll and PhreecRM.dll on windows

---
+++ Tests/autotools.mk
@@ -0,0 +1,65 @@
+LIBDIR =/usr/local/lib
+objects =\
+	AdvectBMI_cpp.o\
+	Advect_c.o\
+	Advect_cpp.o\
+	Gas_c.o\
+	Gas_cpp.o\
+	main.o\
+	SimpleAdvect_c.o\
+	SimpleAdvect_cpp.o\
+	Species_c.o\
+	Species_cpp.o\
+	WriteYAMLFile_cpp.o
+
+cleanfiles =\
+	Advect_c_utility.txt\
+	Advect_c.chem.txt\
+	Advect_c.dmp\
+	Advect_c.log.txt\
+	Advect_cpp_units_utility.txt\
+	Advect_cpp_units_worker.chem.txt\
+	Advect_cpp_units_worker.log.txt\
+	Advect_cpp_units_worker.txt\
+	Advect_cpp_utility.txt\
+	Advect_cpp.chem.txt\
+	Advect_cpp.dmp\
+	Advect_cpp.log.txt\
+	AdvectBMI_cpp_units_utility.txt\
+	AdvectBMI_cpp_units_worker.txt\
+	AdvectBMI_cpp_utility.txt\
+	AdvectBMI_cpp.chem.txt\
+	AdvectBMI_cpp.dmp\
+	AdvectBMI_cpp.log.txt\
+	AdvectBMI_cpp.yaml\
+	Gas_c_utility.txt\
+	Gas_c.chem.txt\
+	Gas_c.dmp\
+	Gas_c.log.txt\
+	Gas_cpp_utility.txt\
+	Gas_cpp.chem.txt\
+	Gas_cpp.dmp\
+	Gas_cpp.log.txt\
+	SimpleAdvect_c.chem.txt\
+	SimpleAdvect_c.log.txt\
+	SimpleAdvect_cpp.chem.txt\
+	SimpleAdvect_cpp.log.txt\
+	Species_c_utility.txt\
+	Species_c.chem.txt\
+	Species_c.dmp\
+	Species_c.log.txt\
+	Species_cpp_utility.txt\
+	Species_cpp.chem.txt\
+	Species_cpp.dmp\
+	Species_cpp.log.txt\
+	Units_Worker.chem.txt\
+	Units_Worker.log.txt\
+	Utility_c.txt\
+	Utility_cpp.out
+
+
+autotools_test : $(objects)
+	$(CXX) -o autotools_test $(objects) -L$(LIBDIR) -lphreeqcrm -lyaml-cpp
+
+clean :
+	rm -f $(objects) $(cleanfiles) test
