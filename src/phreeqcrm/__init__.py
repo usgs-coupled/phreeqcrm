f9c2229af8ca2321f4c9daa0173ed16180ecb129
Charlton, Scott R <charlton@usgs.gov>
2023-07-07 09:55:22 -0600

    WIP

---
+++ src/phreeqcrm/__init__.py
@@ -0,0 +1,13 @@
+from .yamlphreeqcrm import YAMLPhreeqcRM
+from .phreeqcrm import (
+    BMIPhreeqcRM,
+    PhreeqcRM,
+    IRM_BADINSTANCE,
+    IRM_BADVARTYPE,
+    IRM_FAIL,
+    IRM_INVALIDARG,
+    IRM_INVALIDCOL,
+    IRM_INVALIDROW,
+    IRM_OK,
+    IRM_OUTOFMEMORY
+)
